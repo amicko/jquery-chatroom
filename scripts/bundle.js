353d40d628bf6dd043eb667d2f635ada492e57cc
Aaron Micko <gmicko@hotmail.com>
2015-12-23 18:28:27 -0600

    added login, registration, and avdnaced styling for all pages

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -12662,26 +12662,111 @@ return jQuery;
 },{}],4:[function(require,module,exports){
 'use strict';
 var Backbone = require('backbone');
-Parse.initialize('YkFK8qhHXsCDzAxXZcSZjIWjSMZNhrg41yE44hMd', 'oKerLrQEXU3HWO5NiUWSVnwSQKmmg2tFZihnpRHs');
+Parse.initialize("p6LVR6361z3dnHn8SMi8n5Vca7yeUaW4wwXuUnjP", "IFuhIp6PfZJQgP9J3pXECV3M5J42jSkpP7mx9rki");
 
 $(document).ready(function () {
 	var Router = Backbone.Router.extend({
 		routes: {
 			'': 'homepage',
+			'register': 'register',
+			'loginST': 'loginST',
 			'chatroom': 'chatroom'
 		},
 		homepage: function homepage() {
 			$('section').hide();
 			$('.homepage').show();
+			if (Parse.User.current()) {
+				Parse.User.logOut();
+			}
+		},
+		register: function register() {
+			$('section').hide();
+			$('.register').show();
+			$('.primeDirectives').show();
+			$('.internalData').hide();
+			if (Parse.User.current()) {
+				Parse.User.logOut();
+			}
+		},
+		loginST: function loginST() {
+			$('section').hide();
+			$('.loginStarTrek').show();
+			if (Parse.User.current()) {
+				Parse.User.logOut();
+			}
 		},
 		chatroom: function chatroom() {
 			$('section').hide();
 			$('.chatStarTrek').show();
+			if (!Parse.User.current()) {
+				r.navigate('', { trigger: true });
+			}
 		}
 	});
 
 	var r = new Router();
 	Backbone.history.start();
+
+	$('.next1').click(function () {
+		$('.primeDirectives').hide();
+		$('.internalData').show();
+	});
+
+	$('.next2').click(function () {
+		var user = new Parse.User();
+		user.signUp({
+			username: $('#username').val(),
+			password: $('#password1').val(),
+			screenName: $('#screenName').val(),
+			email: $('#email').val(),
+			genre: $('#genre').val(),
+			newsletter: $('#newsletter').val(),
+			birthYear: $('#year').val(),
+			birthMonth: $('#month').val(),
+			birthDay: $('#day').val()
+		}, {
+			success: function success(u) {
+				console.log('User Registered');
+			},
+			error: function error(u, _error) {
+				console.log('User Not Registered');
+			}
+		});
+	});
+
+	$('.back1').click(function () {
+		$('.primeDirectives').show();
+		$('.internalData').hide();
+	});
+
+	$('#loginChatST').click(function () {
+		var user = Parse.User.current();
+		var avatar = $('#loginAvatar').val();
+
+		Parse.User.logIn($('#loginName').val(), $('#loginPassword').val(), {
+			success: function success(u) {},
+			error: function error(u, _error2) {
+				console.log('Error');
+			}
+		});
+
+		setTimeout(function () {
+			Parse.User.current().set('avatar', $('#loginAvatar').val());
+			Parse.User.current().set('handle', $('#loginHandle').val());
+			Parse.User.current().save(null, {
+				success: function success(user) {
+					r.navigate('chatroom', { trigger: true });
+					location.reload();
+				}
+			});
+		}, 500);
+	});
+
+	$('#loginAvatar').click(function () {
+		// console.log($('#loginAvatar').val())
+		console.log('Select Clicked');
+		$('#avvyPic').css('background-image', 'url(../' + $('#loginAvatar').val() + ')');
+	});
 
 	var $text = $('#text');
 	var $send = $('#send');
@@ -12699,10 +12784,19 @@ $(document).ready(function () {
 		var sendMessage = $text.val();
 		var time = new Date();
 
+		var MessageRecipient = '';
+		if ($('#to').val() !== '') {
+			MessageRecipient = $('#to').val() + ': ';
+		} else {
+			MessageRecipient = $('#to').val() + '';
+		}
+
 		var NewMessage = new MessageModel({
-			Title: title,
+			Title: Parse.User.current().get('handle'),
+			To: MessageRecipient,
 			Message: sendMessage,
-			PostDate: time
+			PostDate: time,
+			Image: Parse.User.current().get('avatar')
 		}).save();
 
 		$bottom.html('');
@@ -12716,7 +12810,7 @@ $(document).ready(function () {
 				console.log('Loading...');
 			} else {
 				var MessageList = Messages.map(function (message) {
-					return $bottom.append('<section class="messageBox"><img class="avatar" src=""/><div class="handle">' + message.get('Title') + '</div><div class="time">at ' + message.get('PostDate').toString().substr(16, 8) + '</div><div class="message">' + message.get('Message') + '</div></section>');
+					return $bottom.append('<section class="messageBox"><img class="avatar" src="' + message.get('Image') + '"/><div class="handle">' + message.get('Title') + '</div><div class="time">at ' + message.get('PostDate').toString().substr(16, 8) + '</div><div class="message"><span id="toMessage">' + message.get('To') + '</span> ' + message.get('Message') + '</div></section>');
 				});
 			}
 		});
@@ -12724,9 +12818,32 @@ $(document).ready(function () {
 		$text.val('');
 	});
 
+	$('#receive').click(function () {
+		$bottom.html('');
+		var MessageQuery = new Parse.Query(MessageModel);
+		var Messages = null;
+
+		MessageQuery.descending('createdAt').find().then(function (messages) {
+			Messages = messages;
+
+			if (!Messages) {
+				console.log('Loading...');
+			} else {
+				var MessageList = Messages.map(function (message) {
+					return $bottom.append('<section class="messageBox"><img class="avatar" src="' + message.get('Image') + '"/><div class="handle">' + message.get('Title') + '</div><div class="time">at ' + message.get('PostDate').toString().substr(16, 8) + '</div><div class="message"><span id="toMessage">' + message.get('To') + '</span> ' + message.get('Message') + '</div></section>');
+				});
+			}
+		});
+	});
+
 	$clear.click(function () {
 		$text.val('');
-		$handle.val('');
+		$('#to').val('');
+	});
+
+	$('#exit').click(function () {
+		Parse.User.logOut();
+		r.navigate('', { trigger: true });
 	});
 
 	// setInterval(function() {
@@ -12741,7 +12858,7 @@ $(document).ready(function () {
 			console.log('Loading...');
 		} else {
 			var MessageList = Messages.map(function (message) {
-				return $bottom.append('<section class="messageBox"><img class="avatar" src=""/><div class="handle">' + message.get('Title') + '</div><div class="time">at ' + message.get('PostDate').toString().substr(16, 8) + '</div><div class="message">' + message.get('Message') + '</div></section>');
+				return $bottom.append('<section class="messageBox"><img class="avatar" src="' + message.get('Image') + '"/><div class="handle">' + message.get('Title') + '</div><div class="time">at ' + message.get('PostDate').toString().substr(16, 8) + '</div><div class="message"><span id="toMessage">' + message.get('To') + '</span> ' + message.get('Message') + '</div></section>');
 			});
 		}
 	});
